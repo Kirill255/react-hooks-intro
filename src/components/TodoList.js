c4c02dd328650eb0beb01ed31dbd6de4bbb0a2e5
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-05-28 16:40:44 +0300

    toggle and remove todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,7 @@ import React, { useContext } from "react";
 import TodoContext from "../context";
 
 export default function TodoList() {
-  const { state } = useContext(TodoContext);
+  const { state, dispatch } = useContext(TodoContext);
 
   const title = state.todos.length > 0 ? `${state.todos.length} Todos` : "Nothing To Do!";
   return (
@@ -15,12 +15,18 @@ export default function TodoList() {
             key={todo.id}
             className="flex items-center bg-orange-dark border-black border-dashed border-2 my-2 py-4"
           >
-            <span className="flex-1 ml-12 cursor-pointer">{todo.text}</span>
+            <span
+              className={`flex-1 ml-12 cursor-pointer ${todo.complete &&
+                "line-through text-grey-darkest"}`}
+              onDoubleClick={() => dispatch({ type: "TOGGLE_TODO", payload: todo })}
+            >
+              {todo.text}
+            </span>
             <span className="mr-2">
               <button>
                 <img src="https://icon.now.sh/edit/0050c5" alt="Edit Icon" className="h-6" />
               </button>
-              <button>
+              <button onClick={() => dispatch({ type: "REMOVE_TODO", payload: todo })}>
                 <img src="https://icon.now.sh/delete/8b0000" alt="Delete Icon" className="h-6" />
               </button>
             </span>
