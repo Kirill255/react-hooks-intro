36fc5038539e629db3aac4b1aedfaee8161252b5
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-05-29 17:37:58 +0300

    custom hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+
+import { useTitleInput } from "./hooks/useTitleInput";
 
 const App = () => {
-  const [name, setName] = useState("");
-
-  useEffect(() => {
-    document.title = name;
-  });
+  const [name, setName] = useTitleInput("");
 
   return (
     <div className="main-wrapper">
