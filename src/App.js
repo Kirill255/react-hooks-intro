74b7bdcbd004c537c6bd505d7144371cc284a92a
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-05-29 21:48:12 +0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import Counter from "./Counter";
 
@@ -7,9 +7,21 @@ import { useTitleInput } from "./hooks/useTitleInput";
 const App = () => {
   const [name, setName] = useTitleInput("");
 
+  const reverseWord = (word) => {
+    console.log("function run");
+    return word
+      .split("")
+      .reverse()
+      .join("");
+  };
+
+  const title = "Level Up Dishes";
+
+  const TitleReversed = useMemo(() => reverseWord(title), [title]); // если [title] не изменился, то useMemo не будет повторно запускать reverseWord функцию
+
   return (
     <div className="main-wrapper">
-      <h1>Level Up Dishes</h1>
+      <h1>{TitleReversed}</h1>
       <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
       <h3>{name}</h3>
 
