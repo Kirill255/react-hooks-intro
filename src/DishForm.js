525851b48e10d26b20ad11c79408d96b0cd748f9
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-05-30 09:23:08 +0300

    more complex custom hook

--- src/DishForm.js
+++ src/DishForm.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { useBodyScrollLock } from "./hooks/bodyScrollLock";
+import { useOnClickOutside } from "./hooks/useOnClickOutside";
 
-const DishForm = () => {
+const DishForm = ({ setToggle }) => {
+  const ref = useRef();
+
+  // useOnClickOutside(ref, setToggle); // setToggle будет устанавливать undefined когда элемент скрыт (это тоже false значение, но всё же), поэтому нужно явно передать false, смотреть hooks state в react devtools
+  useOnClickOutside(ref, () => setToggle(false));
+
   useBodyScrollLock();
 
   return (
-    <div className="dish-card">
+    <div className="dish-card" ref={ref}>
       <form>
         <div className="form-row">
           <label htmlFor="name">Name: </label>
---
+++ src/hooks/useOnClickOutside.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+
+const useOnClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = (event) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        console.log(ref.current);
+        return;
+      }
+      handler();
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // [handler, ref]
+};
+
+export { useOnClickOutside };
